be9e4a8378510ff30a079954bd4a1895b196d631
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-30 11:02:41 -0300

    Add Aluno contructor

--- src/Aluno.php
+++ src/Aluno.php
@@ -9,6 +9,13 @@ class Aluno
     private Email $email;
     private array $telefones;
 
+    public function __construct(Cpf $cpf, string $nome, Email $email)
+    {
+        $this->cpf = $cpf;
+        $this->nome = $nome;
+        $this->email = $email;
+    }
+
     public function addTelefone($ddd, $numero)
     {
         $this->telefones[] = new Telefone($ddd, $numero);
