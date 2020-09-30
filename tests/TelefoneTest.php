d9786f84e3aa103989df78bd746c3c1317fd5209
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-30 10:37:29 -0300

    Add Telefones test

---
+++ tests/TelefoneTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Alura\Arquitetura\Tests;
+
+use Alura\Arquitetura\Telefone;
+use PHPUnit\Framework\TestCase;
+
+class TelefoneTest extends TestCase
+{
+    public function testTelefoneDevePoderSerRepresentadoComoString()
+    {
+        $telefone = new Telefone('41', '12345678');
+        $this->assertSame('(41) 12345678', (string) $telefone);
+    }
+    
+    public function testTelefoneComDddInvalidoNaoDevePoderExistir()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectDeprecationMessage('DDD invalido');
+        new Telefone('412', '12345678');
+    }
+
+    public function testTelefoneComNumeroInvalidoNaoDevePoderExistir()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectDeprecationMessage('Numero de telefone invalido');
+        new Telefone('41', 'numero');
+    }
+}
