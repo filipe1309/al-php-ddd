220b7ecdd8297a3361c533221b036217a8b4d305
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-30 10:37:49 -0300

    Add Telefone Value Object

---
+++ src/Telefone.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Alura\Arquitetura;
+
+class Telefone
+{
+    private string $ddd;
+    private string $numero;
+
+    public function __construct(string $ddd, string $numero)
+    {
+        $this->setDdd($ddd);
+        $this->setNumero($numero);
+    }
+
+    private function setDdd($ddd): void
+    {
+        // Guard clause
+        if (preg_match('/^\d{2}$/', $ddd) !== 1) {
+            throw new \InvalidArgumentException('DDD invalido');
+        }
+
+        $this->ddd = $ddd;
+    }
+
+    private function setNumero($numero): void
+    {
+        // Guard clause
+        if (preg_match('/^\d{8,9}$/', $numero) !== 1) {
+            throw new \InvalidArgumentException('Numero de telefone invalido');
+        }
+
+        $this->numero = $numero;
+    }
+
+    public function __toString(): string
+    {
+        return "({$this->ddd}) {$this->numero}";
+    }
+}
