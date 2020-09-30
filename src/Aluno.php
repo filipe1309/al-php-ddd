ea4741c910af4f63181c846ffac2f0646129241f
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-30 10:38:25 -0300

    Add method to create new Telefone into Aluno

--- src/Aluno.php
+++ src/Aluno.php
@@ -7,4 +7,10 @@ class Aluno
     private Cpf $cpf;
     private string $nome;
     private Email $email;
+    private array $telefones;
+
+    public function addTelefone($ddd, $numero)
+    {
+        $this->telefones[] = new Telefone($ddd, $numero);
+    }
 }
