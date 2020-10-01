8a0eaee38002b432d8e44b02d9cd61dfa109c28a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-01 16:49:05 -0300

    Add Use Case

---
+++ src/Aplicacao/Aluno/MatricularAluno/MatricularAluno.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno;
+
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
+
+class MatricularAluno
+{
+    private RepositorioDeAluno $repositorioDeAluno;
+
+    public function __construct(RepositorioDeAluno $repositorioDeAluno)
+    {
+        $this->repositorioDeAluno = $repositorioDeAluno;
+    }
+
+    public function executa(MatricularAlunoDto $dados): void
+    {
+        $aluno = Aluno::comCpfNomeEEmail($dados->cpfAluno, $dados->nomeAluno, $dados->emailAluno);
+        $this->repositorioDeAluno ->adicionar($aluno);
+    }
+}
