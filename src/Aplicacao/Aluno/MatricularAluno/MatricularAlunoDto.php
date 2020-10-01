bfebddc9ada191d09f584b02a2a6496145272b52
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-01 16:48:58 -0300

    Add DTO

---
+++ src/Aplicacao/Aluno/MatricularAluno/MatricularAlunoDto.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno;
+
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
+
+class MatricularAlunoDto
+{
+    public string $cpfAluno;
+    public string $nomeAluno;
+    public string $emailAluno;
+
+    public function __construct(string $cpfAluno, string $nomeAluno, string $emailAluno)
+    {
+        $this->cpfAluno = $cpfAluno;
+        $this->nomeAluno = $nomeAluno;
+        $this->emailAluno = $emailAluno;
+    }
+}
