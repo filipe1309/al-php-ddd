fe7e38235622c4654ecb7ad7a4570f46a032f5e4
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-02 11:48:47 -0300

    Improve AlunoTest test

--- tests/Dominio/Aluno/AlunoTest.php
+++ tests/Dominio/Aluno/AlunoTest.php
@@ -2,17 +2,44 @@
 
 namespace Alura\Arquitetura\Tests\Dominio\Aluno;
 
-use Alura\Arquitetura\Dominio\Aluno\Aluno;
 use PHPUnit\Framework\TestCase;
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\{ Cpf, Email };
 use Alura\Arquitetura\Dominio\Aluno\AlunoCom2TelefonesException;
 
 class AlunoTest extends TestCase
 {
+    private Aluno $aluno;
+
+    public function setUp(): void
+    {
+        $this->aluno = new Aluno(
+            $this->createStub(Cpf::class),
+            '',
+            $this->createStub(Email::class),
+        );
+    }
+
     public function testAlunoNaoPodeTerMaisQue2Telefones()
     {
         $this->expectException(AlunoCom2TelefonesException::class);
+ 
         $this->expectDeprecationMessage('Aluno ja tem 2 telefones, nao eh possivel add outro.');
         $aluno = Aluno::comCpfNomeEEmail('012.345.678-90', 'Bob Dylan', 'test@example.com');
         $aluno->adicionarTelefone('41', '123465789')->adicionarTelefone('41', '12346578')->adicionarTelefone('41', '987654321');
+    }
+
+    public function testAdicionar1TelefoneDeveFuncionar()
+    {
+        $this->aluno->adicionarTelefone('41', '123465789');
+
+        $this->assertCount(1, $this->aluno->telefones());
+    }
+
+    public function testAdicionar2TelefonesDeveFuncionar()
+    {
+        $this->aluno->adicionarTelefone('41', '123465789')->adicionarTelefone('41', '12346578');
+
+        $this->assertCount(2, $this->aluno->telefones());
     }
 }
