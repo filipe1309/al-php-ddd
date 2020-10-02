f2034414a948e07a65f36f933c573589f553c006
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-02 15:19:20 -0300

    Improve test script

--- matricular-aluno.php
+++ matricular-aluno.php
@@ -3,14 +3,25 @@
 require 'vendor/autoload.php';
 
 use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\PublicadorDeEvento;
+use Alura\Arquitetura\Dominio\Aluno\LogDeAlunoMatriculado;
 use Alura\Arquitetura\Infra\Aluno\RepositorioDeAlunoEmMemoria;
+use Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno\MatricularAluno;
+use Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno\MatricularAlunoDto;
 
 $cpf    = $argv[1];
 $nome   = $argv[2];
 $email  = $argv[3];
-$ddd    = $argv[4];
-$numero = $argv[5];
+// $ddd    = $argv[4];
+// $numero = $argv[5];
 
-$aluno = Aluno::comCpfNomeEEmail($cpf, $nome, $email)->adicionarTelefone($ddd, $numero);
+// $aluno = Aluno::comCpfNomeEEmail($cpf, $nome, $email)->adicionarTelefone($ddd, $numero);
+// $repositorio = new RepositorioDeAlunoEmMemoria();
+// $repositorio->adicionar($aluno);
+
 $repositorio = new RepositorioDeAlunoEmMemoria();
-$repositorio->adicionar($aluno);
+$publicador = new PublicadorDeEvento();
+$publicador->adicionarOuvinte(new LogDeAlunoMatriculado());
+$useCase = new MatricularAluno($repositorio, $publicador);
+
+$useCase->executa(new MatricularAlunoDto($cpf, $nome, $email));
