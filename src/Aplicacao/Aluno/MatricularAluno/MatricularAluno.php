cbd24234ee79ad1f996979df006af28959826226
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-02 15:18:21 -0300

    Add Event trigger

--- src/Aplicacao/Aluno/MatricularAluno/MatricularAluno.php
+++ src/Aplicacao/Aluno/MatricularAluno/MatricularAluno.php
@@ -3,20 +3,32 @@
 namespace Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno;
 
 use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\PublicadorDeEvento;
+use Alura\Arquitetura\Dominio\Aluno\AlunoMatriculado;
 use Alura\Arquitetura\Dominio\Aluno\RepositorioDeAluno;
+use Alura\Arquitetura\Dominio\Aluno\LogDeAlunoMatriculado;
 
 class MatricularAluno
 {
     private RepositorioDeAluno $repositorioDeAluno;
+    private PublicadorDeEvento $publicador;
 
-    public function __construct(RepositorioDeAluno $repositorioDeAluno)
+    public function __construct(RepositorioDeAluno $repositorioDeAluno, PublicadorDeEvento $publicador)
     {
         $this->repositorioDeAluno = $repositorioDeAluno;
+
+        // In real world PublicadorDeEvento will be injected with DI, as a parameter
+        // $this->publicador = new PublicadorDeEvento();
+        // $this->publicador->adicionarOuvinte(new LogDeAlunoMatriculado());
+        $this->publicador = $publicador;
     }
 
     public function executa(MatricularAlunoDto $dados): void
     {
         $aluno = Aluno::comCpfNomeEEmail($dados->cpfAluno, $dados->nomeAluno, $dados->emailAluno);
         $this->repositorioDeAluno ->adicionar($aluno);
+
+        $evento = new AlunoMatriculado($aluno->cpf());
+        $this->publicador->publicar($evento);
     }
 }
